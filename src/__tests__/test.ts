07bd812b23cefb6b75bfd48b3f83de66624773a9
arm <amirrezamohammadi1374@gmail.com>
2022-06-04 18:50:14 +0430

    preparing for publish

---
+++ src/__tests__/test.ts
@@ -0,0 +1,4 @@
+import { ViraLink } from '../main';
+test('My mock', () => {
+  expect('Hello Carl').toBe('Hello Carl');
+});
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,56 @@
-import * as mqtt from 'mqtt'
+import * as mqtt from 'mqtt';
+import { MqttClient } from 'mqtt';
 
-var rpcReqId = 0;
+let rpcReqId = 0;
 
-export class Viralink {
+export class ViraLink {
+  client: MqttClient;
 
-    client: any
+  constructor() {
+    this.client = mqtt.connect('mqtt://console.viralink.io', {
+      username: 'kpC9YYIm2lvmxgfoQwsx',
+    });
 
-    constructor() {
-        this.client = mqtt.connect('mqtt://console.viralink.io', {
-            username: 'kpC9YYIm2lvmxgfoQwsx'
-        });
+    this.client.on('connect', () => {
+      // console.log('connected');
+      this.client.subscribe([
+        'v1/devices/me/rpc/request/+',
+        'v1/devices/me/rpc/response/+',
+        'v1/devices/me/attributes',
+        'v1/devices/me/attributes/response/+',
+      ]);
+    });
 
-        this.client.on('connect', () => {
-            console.log('connected');
-            this.client.subscribe(['v1/devices/me/rpc/request/+', 'v1/devices/me/rpc/response/+', 'v1/devices/me/attributes', 'v1/devices/me/attributes/response/+'])
-        });
-
-        this.client.on('message', (topic: string, message: string) => {
-            console.log('request.topic: ' + topic);
-            console.log('request.body: ' + message.toString());
-            if (topic.includes('v1/devices/me/rpc')) {
-                const requestId = topic.slice('v1/devices/me/rpc/request/'.length);
-                if (topic.includes('v1/devices/me/rpc/request')) {
-                    this.client.publish('v1/devices/me/rpc/response/' + requestId, message);
-                } else if (topic.includes('v1/devices/me/rpc/response')) {
-
-                }
-            } else if (topic.includes('v1/devices/me/attributes')) {
+    this.client.on('message', (topic: string, message: string) => {
+      // console.log('request.topic: ' + topic);
+      // console.log('request.body: ' + message.toString());
+      if (topic.includes('v1/devices/me/rpc')) {
+        const requestId = topic.slice('v1/devices/me/rpc/request/'.length);
+        if (topic.includes('v1/devices/me/rpc/request'))
+          this.client.publish('v1/devices/me/rpc/response/' + requestId, message);
+        // } else if (topic.includes('v1/devices/me/rpc/response')) {
+        //
+        // }
+      } /*else if (topic.includes('v1/devices/me/attributes')) {
                 const requestId = topic.slice('v1/devices/me/attributes'.length);
                 if (topic.includes('v1/devices/me/attributes/response/')) {
 
                 } else if (topic.includes('v1/devices/me/attributes')) {
 
                 }
-            }
-        });
-    }
+            }*/
+    });
+  }
 
-    sendTelemetry(payload: string) {
-        this.client.publish('v1/devices/me/telemetry', payload);
-    }
+  sendTelemetry(payload: string) {
+    this.client.publish('v1/devices/me/telemetry', payload);
+  }
 
-    sendAttributes(payload: string) {
-        this.client.publish('v1/devices/me/attributes', payload);
-    }
+  sendAttributes(payload: string) {
+    this.client.publish('v1/devices/me/attributes', payload);
+  }
 
-    sendRPC(payload: string) {
-        this.client.publish('v1/devices/me/rpc/request/' + rpcReqId++, payload);
-    }
-
-
+  sendRPC(payload: string) {
+    this.client.publish('v1/devices/me/rpc/request/' + rpcReqId++, payload);
+  }
 }
-
